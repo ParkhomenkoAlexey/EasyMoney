9a1f5af4cdd0f760f5deebb47237d87186efc16e
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-03-30 11:12:37 +0500

    Initial Commit

---
+++ EasyMoney/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyMoney
+//
+//  Created by Алексей Пархоменко on 30.03.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
